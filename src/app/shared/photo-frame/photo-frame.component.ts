d03bcd0102259cd32ee4dcd5d4e5b9ef86da703c
Cauê Almeida <nncl@live.com>
2021-12-08 22:42:40 -0300

    feat(components): Creating photo-frame.component.html

--- src/app/shared/photo-frame/photo-frame.component.ts
+++ src/app/shared/photo-frame/photo-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-photo-frame',
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: [ './photo-frame.component.scss' ]
 })
 export class PhotoFrameComponent {
+  @Output() liked: EventEmitter<void> = new EventEmitter<void>();
+
+  @Input() description = '';
+  @Input() src = '';
+  @Input() likes = 0;
+
+  public like(): void {
+    this.liked.emit();
+  }
 }
