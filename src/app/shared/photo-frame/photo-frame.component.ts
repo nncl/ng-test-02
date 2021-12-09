5cc853728a51b399ef07ebd3c52ffd10bc74d8dd
Cauê Almeida <nncl@live.com>
2021-12-08 23:00:29 -0300

    feat(components): Debouncing like button action

--- src/app/shared/photo-frame/photo-frame.component.ts
+++ src/app/shared/photo-frame/photo-frame.component.ts
@@ -1,18 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: [ './photo-frame.component.scss' ]
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() liked: EventEmitter<void> = new EventEmitter<void>();
 
   @Input() description = '';
   @Input() src = '';
   @Input() likes = 0;
 
+  private debounceSubject: Subject<void> = new Subject<void>();
+  private unsubscribe: Subject<void> = new Subject<void>();
+
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => this.liked.emit());
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
