6e13760baf1770b81637f1e180a56575b5d90eb6
Cauê Almeida <nncl@live.com>
2021-12-09 22:58:59 -0300

    test(components): Implementing fakeAsync and tick

--- src/app/shared/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/photo-frame/photo-frame.component.spec.ts
@@ -1,5 +1,5 @@
 import { PhotoFrameComponent } from './photo-frame.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { PhotoFrameModule } from './photo-frame.module';
 
 describe(`${PhotoFrameComponent.name}`, () => {
@@ -21,7 +21,7 @@ describe(`${PhotoFrameComponent.name}`, () => {
 
   it(`#${PhotoFrameComponent.prototype.like.name}
   should trigger (@Output liked) once when called multiple times within debounce time`,
-    () => {
+    fakeAsync(() => {
       fixture.detectChanges();
       let times = 0;
       component.liked.subscribe(() => times++);
@@ -29,6 +29,23 @@ describe(`${PhotoFrameComponent.name}`, () => {
       component.like();
       component.like();
 
+      tick(500);
+
       expect(times).toBe(1);
-    });
+    }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  should trigger (@Output liked) twice when called outside debounce time`,
+    fakeAsync(() => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+
+      component.like();
+      tick(500);
+      component.like();
+      tick(500);
+
+      expect(times).toBe(2);
+    }));
 });
