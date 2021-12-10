b2863ca7ccc703e2ef5f29a7158ac2ea0ee5f7d5
Cauê Almeida <nncl@live.com>
2021-12-09 22:51:49 -0300

    test(components): Testing liked event

--- src/app/shared/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/photo-frame/photo-frame.component.spec.ts
@@ -18,4 +18,17 @@ describe(`${PhotoFrameComponent.name}`, () => {
   it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  should trigger (@Output liked) once when called multiple times within debounce time`,
+    () => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+
+      component.like();
+      component.like();
+
+      expect(times).toBe(1);
+    });
 });
