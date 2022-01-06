2576a238ef600dfcd3413e002138810c5b036954
Caue <nncl@live.com>
2022-01-05 23:55:02 -0300

    test(components): Setting up PhotoList test

---
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -0,0 +1,22 @@
+import { PhotoListComponent } from './photo-list.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PhotoListModule } from './photo-list.module';
+import { HttpClientModule } from '@angular/common/http';
+
+describe(PhotoListComponent.name, () => {
+  let fixture: ComponentFixture<PhotoListComponent>;
+  let component: PhotoListComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ PhotoListModule, HttpClientModule ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PhotoListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it(`should create component`, () => {
+    expect(component).toBeTruthy();
+  });
+});
