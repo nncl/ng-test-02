f3364e6635936903b092f281cf18537b428ad034
Cauê Almeida <nncl@live.com>
2022-01-10 22:49:09 -0300

    test(components): Injecting Service

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -2,10 +2,12 @@ import { PhotoListComponent } from './photo-list.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoListModule } from './photo-list.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoBoardService } from '../../shared/components/photo-board/services/photo-board.service';
 
 describe(PhotoListComponent.name, () => {
   let fixture: ComponentFixture<PhotoListComponent>;
   let component: PhotoListComponent;
+  let service: PhotoBoardService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +16,14 @@ describe(PhotoListComponent.name, () => {
 
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(PhotoBoardService);
   });
 
   it(`should create component`, () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should display board when data arrives`, () => {
+    // TODO
   });
 });
