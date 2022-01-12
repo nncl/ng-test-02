ea1b962811a0e1f3de5007382eef9da6375eac7a
Cauê Almeida <nncl@live.com>
2022-01-11 22:19:40 -0300

    feat(services): Intercepting photos' description result

--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Photo } from '../interfaces/photo';
-import { delay } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoBoardService {
@@ -11,6 +11,12 @@ export class PhotoBoardService {
 
   public getPhotos(): Observable<Photo[]> {
     return this.httpClient.get<Photo[]>(`http://localhost:3000/photos`)
+      .pipe(map(photos => {
+        return photos.map(photo => {
+          return { ...photo, description: photo.description.toUpperCase() };
+        });
+      }))
+      .pipe(tap(console.log))
       .pipe(delay(2000));
   }
 }
