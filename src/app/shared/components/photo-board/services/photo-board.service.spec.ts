57c89dcbc32e5794c2aad95ec6f274b0a7db942c
Cauê Almeida <nncl@live.com>
2022-01-11 22:35:37 -0300

    test(services): Mocking getPhotos data

--- src/app/shared/components/photo-board/services/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -2,6 +2,14 @@ import { PhotoBoardService } from './photo-board.service';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    { id: 1, description: 'Example 1', src: '' },
+    { id: 2, description: 'Example 2', src: '' },
+  ]
+};
+
 describe(PhotoBoardService.name, () => {
   let service: PhotoBoardService;
   let httpController: HttpTestingController;
@@ -14,6 +22,21 @@ describe(PhotoBoardService.name, () => {
 
     service = TestBed.inject(PhotoBoardService);
     httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it(`${PhotoBoardService.prototype.getPhotos.name} should return list photos with uppercase description`, done => {
+
+    service.getPhotos().subscribe(photos => {
+      expect(photos[0].description).toBe('EXAMPLE 1');
+      expect(photos[1].description).toBe('EXAMPLE 2');
+
+      done();
+    });
+
+    httpController
+      .expectOne(mockData.api)
+      .flush(mockData.data);
+
   });
 });
 
